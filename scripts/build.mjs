a1387f0c38aac355f3e0a5e1efdeb973f75f9a92
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2022-03-19 21:06:18 +0200

    remove watch script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,13 +1,10 @@
 import { build, analyzeMetafile } from "esbuild";
 import { dtsPlugin as dts } from "esbuild-plugin-d.ts";
 
-const watch = !!process.argv.find(v => v === "watch");
-
 const common = {
   entryPoints: [ "./src/extend-event-target.js" ],
   platform: "browser",
   bundle: true,
-  watch,
 }
 
 build({
